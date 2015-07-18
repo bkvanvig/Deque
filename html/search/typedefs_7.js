cf61ddd5fe0d02b6ebf752d0e7f5e4b5898fbcf5
bkvanvig <bkvanvig104@yahoo.com>
2015-07-17 19:06:43 -0500

    created Doxygen documentation, close #18

---
+++ html/search/typedefs_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['size_5ftype',['size_type',['../classmy__deque.html#a61e5e5317fe72a381ce4d45f09544b02',1,'my_deque::size_type()'],['../structDeque__Fixture.html#aaf7d8d8eda0003cc8727d9eab7f22086',1,'Deque_Fixture::size_type()']]]
+];
---
+++ html/search/variables_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['outer_5fb',['outer_b',['../classmy__deque.html#afe69687aa94aa63dbb23e147a90db3d1',1,'my_deque']]],
+  ['outer_5fe',['outer_e',['../classmy__deque.html#a4d7496e28dd1a7bda7df4c6861b1f5c1',1,'my_deque']]],
+  ['outer_5fsize',['OUTER_SIZE',['../classmy__deque.html#a27647dca34c708d1b4705ede6d46fd7e',1,'my_deque']]]
+];
